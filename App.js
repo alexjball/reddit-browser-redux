fb5e8618c83d2da2aaf90d77ccc48cc65e11975d
Alex Ball <alex.ball@cantina.co>
2020-05-27 19:35:58 -0400

    Fix top-level padding

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { SafeAreaView, Platform, StatusBar, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 
 import store from './state';
-import Browser from './views/layouts/browser';
+import Browser from './views/layouts/Browser';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Browser />
+      <SafeAreaView
+        style={[
+          styles.container,
+          Platform.OS === 'android' && { paddingTop: StatusBar.currentHeight },
+        ]}
+      >
+        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+
+        <Browser />
+      </SafeAreaView>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
